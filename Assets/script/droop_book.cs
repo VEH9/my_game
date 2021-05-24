4648a5fb07cf95623ad8423fb16ea359e2944c55
Ivan <ivan.s2002.mail@gmail.com>
2021-05-24 14:36:41 +0500

    fix script

--- Assets/script/droop_book.cs
+++ Assets/script/droop_book.cs
@@ -7,14 +7,13 @@ public class droop_book : MonoBehaviour
 {
     public Vector2 vector;
     public GameObject book;
+    private bool complite = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
-        if (other.CompareTag("Player"))
-        {
-            book.transform.position = vector;
-            //MainCamera.SetActive(!MainCamera.activeSelf);
-        }
+        if (!other.CompareTag("Player") || complite) return;
+        complite = true;
+        book.transform.position = vector;
+        //MainCamera.SetActive(!MainCamera.activeSelf);
 
         //SecondCamera.SetActive(!SecondCamera.activeSelf);
     }
