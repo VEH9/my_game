78aa4fc6216da0f4c4a0fd79b86d117b78dcdb0e
Ivan <ivan.s2002.mail@gmail.com>
2021-05-28 12:27:25 +0500

    asd

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -10,26 +10,57 @@ public class Enemy : MonoBehaviour
     private int health;
     [SerializeField]
     private float speed = 2.0F;
-
+    
+    [SerializeField]
+    public Unit player;
     private bool dead;
+    public int damage;
+    public LayerMask enemy;
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
 
-    //public GameObject qwe;
     private CharStateWolf State
     {
         get { return (CharStateWolf)animator.GetInteger("State"); }
         set { animator.SetInteger("State", (int)value); }
     }
+
     // Update is called once per frame
     void Update()
     {
         if (health <= 0 && !dead)
         {
+            dead = true;
             State = CharStateWolf.Death;
            // Destroy(gameObject);
-            dead = true;
         }
+        
+       
+        timeBtwAttack -= Time.deltaTime;
+        
+        float direction = player.transform.position.x - transform.position.x;
+        if (Mathf.Abs(direction) < 5 && !dead)
+        {
+            Vector3 pos = transform.position;
+            pos.x += Mathf.Sign(direction) * speed * Time.deltaTime;
+            sprite.flipX = direction > 0.0F;
+            State = CharStateWolf.Run;
+            transform.position = pos;
+            
+        }
+
         //движение
     }
+
+    /*    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && timeBtwAttack <= 0) 
+        {
+            Debug.Log("кусь");
+            //player.ReceiveDamage();
+            timeBtwAttack = startTimeBtwAttack;
+        }
+    }*/
 
     void Deadd()
     {
@@ -37,9 +68,10 @@ public class Enemy : MonoBehaviour
     }
     
     private Animator animator;
-
+    private SpriteRenderer sprite;
     private void Awake()
     {
+        sprite = GetComponentInChildren<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
 
